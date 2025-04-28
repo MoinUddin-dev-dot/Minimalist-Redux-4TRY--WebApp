d6d21ed07b8619165036d5b635fcdbdc2cf24278
Moin UD-din <129534945+MoinUddin-dev-dot@users.noreply.github.com>
2025-04-28 11:32:25 +0500

    fixes

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import quoteReducer from '../quotes/quotes'
 import emojiReducer from '../emojis/EmojiSlice'
-
 export default configureStore({
   reducer: {
     quotes: quoteReducer,
